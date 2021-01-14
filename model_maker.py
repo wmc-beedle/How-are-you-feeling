ad99eb141884f9f8d802e5d318af418793c16237
wmc-beedle <williamcbeedle@gmail.com>
2021-01-14 14:25:58 -0800

    attempt to get confusion matrix

--- model_maker.py
+++ model_maker.py
@@ -8,25 +8,26 @@ from tqdm import tqdm
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
 from tensorflow.keras.layers import Conv2D, MaxPooling2D
-from image_pre import process
+from library import featurize
 from sklearn.model_selection import train_test_split
 
-emotions = {'Peace':0, 'Affection':1, 'Esteem':2, 'Anticipation':3, 'Engagement':4,
-             'Confidence':5, 'Happiness':6, 'Pleasure':7, 'Excitement':8,'Surprise':9,
-             'Sympathy':10, 'Doubt/Confusion':11, 'Disconnection':12, 'Fatigue':13, 'Embarrassment':14,
-             'Yearning':15, 'Disapproval':16, 'Aversion':17, 'Annoyance':18, 'Anger':19, 
-             'Sensitivity':20, 'Sadness':21, 'Disquietment':22, 'Fear':23, 'Pain':24, 'Suffering':25}
-genders = {'Female':0,'Male':1}
-ages = {'Adult':0, 'Teenager':1, 'Kid':2}
+# Original Full List of Emotions for future modellin
+# emotions = {'Peace':0, 'Affection':1, 'Esteem':2, 'Anticipation':3, 'Engagement':4,
+#              'Confidence':5, 'Happiness':6, 'Pleasure':7, 'Excitement':8,'Surprise':9,
+#              'Sympathy':10, 'Doubt/Confusion':11, 'Disconnection':12, 'Fatigue':13, 'Embarrassment':14,
+#              'Yearning':15, 'Disapproval':16, 'Aversion':17, 'Annoyance':18, 'Anger':19, 
+#              'Sensitivity':20, 'Sadness':21, 'Disquietment':22, 'Fear':23, 'Pain':24, 'Suffering':25}
 
 train_file = 'Data/emotic_pre/train.csv'
 test_file = 'Data/emotic_pre/test.csv'
 
-X, Y = process(train_file)
+X, Y = featurize(train_file,test_file)#Condensing Train and test set to try to streamline the incorporation into the model 
 
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3) #Splitting the data into train and test splits
 
+
+# Creating the model
 model = Sequential()
 model.add(Conv2D(32,(3, 3), activation='relu',input_shape=(48,48,1)))
 model.add(MaxPooling2D(2, 2))
@@ -40,8 +41,9 @@ model.add(MaxPooling2D(2, 2))
 model.add(Conv2D(256,(3, 3), activation='relu'))
 model.add(MaxPooling2D(2, 2))
 
-model.add(Flatten())
+model.add(Flatten()) # Flatten to go to conventional layer
 model.add(Dense(4))
+model.add(Dropout(0,5)) # try to work against overfitting
 
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
@@ -52,4 +54,4 @@ test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=0)
 elapsed = timeit.default_timer() - start_time
 print("\nTest data, accuracy: {:5.2f}%".format(100*test_acc))
 print('\nTook {:.2f}s to finish'.format(elapsed))
-# model.save('./emotion_detector_CNN.model')
+model.save('./emotion_detector_CNN.model') # Save your model!
